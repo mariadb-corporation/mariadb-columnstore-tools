dae5e3a6ecceaf47ba75e7495f494de18b96d159
sneJ- <jens.rowekamp@mariadb.com>
2018-10-08 15:01:09 -0700

    MCOL-1242 restored and adapted package.sh to don't include mcsimport, cmake and build directories

---
+++ package.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+. VERSION
+
+subDirs=$(find . -maxdepth 1 -mindepth 1 -type d ! -iname ".*" ! -iname "mcsimport" ! -iname "build" ! -iname "resources" ! -iname "cmake" )
+
+tar -zcvf mariadb-columnstore-tools-$COLUMNSTORE_VERSION_MAJOR.$COLUMNSTORE_VERSION_MINOR.$COLUMNSTORE_VERSION_PATCH-$COLUMNSTORE_VERSION_RELEASE.bin.tar.gz COPYRIGHT.txt LICENSE.txt README.md VERSION $subDirs
+
